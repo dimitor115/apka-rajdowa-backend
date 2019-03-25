eda9ced06f3b263d8bbc62ccc644906847afea81
Maciej Kopeć <dimitor115@gmail.com>
2019-03-25 21:22:51 +0100

    delete useless files

--- server/routes.js
+++ server/routes.js
@@ -1,8 +1,6 @@
 import examplesApi from './controllers/ExamplesApi'
-import eventsApi from './controllers/EventsApi'
 
 // Here we define next api sections for next controllers
 export default function routes(app) {
   app.use('/api/v1/examples', examplesApi)
-  app.use('/api/v1/events', eventsApi)
 }
