f628d667f6f4935f90964a4d1d3c24b5b99bb63b
Mitelak <mitelak@gmail.com>
2019-04-05 02:09:24 +0200

    Add redirect after auth callback

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -5,10 +5,15 @@ import { authentication, authorization } from '../middlewares'
 const router = express.Router()
 
 router.get('/google', authentication)
+
 router.get('/google/callback', authentication, (req, res) => {
   const token = createToken(req.user)
-  res.status(req.authInfo || 500).send({ token })
+  // eslint-disable-next-line max-len
+  // TODO: Na razie zahardkodowane trzeba by było zrobić to jako parametr 'redirect' z frontu czy coś takiego
+  // Swoją drogą ta strategia korzysta z google+ xD
+  res.redirect(`http://localhost:3000/login?token=${token}`)
 })
+
 router.get('/test', authorization, (req, res) => res.send('elo'))
 
 export default router
