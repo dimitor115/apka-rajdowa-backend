dba4ef52c4746e6b42a302fb3585a99bf59b7428
waszkowski <marek.waszkowski@gmail.com>
2019-04-06 11:38:40 +0200

    feat(AR): dodanie corsow, poprawienie naglowkow dla participantsController

--- server/controllers/ParticipantsController.js
+++ server/controllers/ParticipantsController.js
@@ -3,9 +3,9 @@ import resultHandler from '../middlewares/resultHandler'
 import { ParticipantsService } from '../services'
 
 const router = express.Router()
-router.post('/', resultHandler(req => ParticipantsService.add(req.headers.form_id, 'private', req.body)))
-router.post('/form', resultHandler(req => ParticipantsService.add(req.headers.form_id, 'public', req.body)))
-router.patch('/', resultHandler(req => ParticipantsService.edit(req.headers.form_id, req.query, req.body)))
-router.get('/', resultHandler(req => ParticipantsService.find(req.headers.form_id, req.query)))
+router.post('/', resultHandler(req => ParticipantsService.add(req.header('form-id'), 'private', req.body)))
+router.post('/form', resultHandler(req => ParticipantsService.add(req.header('form-id'), 'public', req.body)))
+router.patch('/', resultHandler(req => ParticipantsService.edit(req.header('form-id'), req.query, req.body)))
+router.get('/', resultHandler(req => ParticipantsService.find(req.header('form-id'), req.query)))
 
 export default router
