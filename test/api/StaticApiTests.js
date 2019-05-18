abde24b374eaec4194369c8e951a2a65764ffdaf
Maciej Kopeć <dimitor115@gmail.com>
2019-05-18 15:34:25 +0200

    fix test

--- test/api/StaticApiTests.js
+++ test/api/StaticApiTests.js
@@ -1,6 +1,7 @@
 import { expect } from 'chai'
 import { describe } from 'mocha'
 import request from 'request-promise-native'
+import apiUrl from './apiUrl'
 import { insertEvent, removeEvent, event } from './EventsApiTests'
 
 describe('Static Api', () => {
@@ -12,11 +13,13 @@ describe('Static Api', () => {
         // given
         const { logo } = insertResult.data
         // when
-        const img = await request(logo)
+        const img = await request(apiUrl(`/api/v1${logo}`))
         // then
         expect(img).to.exist
         // after
         const removeResult = await removeEvent(insertResult.data._id)
-        expect(removeResult.data._id).to.equal(insertResult.data._id)
+        expect(removeResult.data._id)
+            .to
+            .equal(insertResult.data._id)
     })
 })
